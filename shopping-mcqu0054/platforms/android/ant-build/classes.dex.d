e2f48ded8d6f746a03550443f940914d8b253c1f
Tia McQuaid <mcqu0054@algonquinlive.com>
2014-12-10 20:36:16 -0500

    commit

---
+++ shopping-mcqu0054/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes.dex : \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\BuildConfig.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\CordovaApp.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\R$attr.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\R$drawable.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\R$string.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\R$xml.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\classes\ca\edumedia\m\mcqu0054\shopping\R.class \
+C:\wamp\www\mad9014\shopping-mcqu0054\platforms\android\ant-build\dexedLibs\classes-83a46748d268350d3349c7e3c1b9102e.jar \
